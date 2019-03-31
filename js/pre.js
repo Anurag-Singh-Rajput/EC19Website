4dbf31b4e6983d9f11a94c58403e5c5dc2451db4
Xavier <saagarverma1234@gmail.com>
2019-03-31 22:39:11 +0530

    added dance and automobiles pics

--- js/pre.js
+++ js/pre.js
@@ -1,4 +1,4 @@
-var images = ["images/bg/home.jpg","images/bg/drama.jpg","images/bg/contact.jpeg","images/bg/sponsor.jpg","images/bg/ec_back_2","images/bg/ec_back"];
+var images = ["images/bg/home2.jpg","images/bg/drama.jpg","images/bg/contact.jpg","images/bg/sponsors.jpg","images/bg/ec_back_2","images/bg/ec_back","images/bg/event.jpg","images/bg/register.jpg"];
 var x1 = 0;
 var percent = 0;
 var i = 0;
